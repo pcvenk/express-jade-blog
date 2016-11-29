ef58d0bd74ba3a3bca33c6083586c24d90ecfb2d
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-29 21:07:12 +0100

    Done: redirect to home view

--- routes/manage.js
+++ routes/manage.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+router.get('/articles', function (req, res, next) {
+    res.render('manage-articles', {title: 'Manage Articles'});
 });
 
 module.exports = router;

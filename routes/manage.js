55b5087818070cf3fe034e2498b65c63939f1df2
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-29 21:48:49 +0100

    Done: edit categories and edit articles view

--- routes/manage.js
+++ routes/manage.js
@@ -5,4 +5,8 @@ router.get('/articles', function (req, res, next) {
     res.render('manage-articles', {title: 'Manage Articles'});
 });
 
+router.get('/categories', function (req, res, next) {
+    res.render('manage-categories', {title: 'Manage Categories'});
+});
+
 module.exports = router;

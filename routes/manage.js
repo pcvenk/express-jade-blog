18f21ee6014cb1308b9f9084a78d3436de8425f1
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-30 15:54:54 +0100

    Added add category and add article view

--- routes/manage.js
+++ routes/manage.js
@@ -9,4 +9,12 @@ router.get('/categories', function (req, res, next) {
     res.render('manage-categories', {title: 'Manage Categories'});
 });
 
+router.get('/articles/add', function(req, res, next){
+   res.render('add-article', {title: 'Add Article'})
+});
+
+router.get('/categories/add', function(req, res, next){
+   res.render('add-category', {title: 'Add Category'})
+});
+
 module.exports = router;

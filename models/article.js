f8824a0015f80c6ec840cd79e9b9ce264eec705a
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-12-05 18:01:40 +0100

    Added the article model

--- models/article.js
+++ models/article.js
@@ -1,3 +1,25 @@
-/**
- * Created by Primoz on 30/11/2016.
- */
+var mongoose = require('mongoose');
+
+var articleSchema = new mongoose.Schema({
+
+    title: String,
+    subtitle: String,
+    category: String,
+    body: String,
+    author: String,
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    comments: [{
+
+        commentSubject: String,
+        commentBody: String,
+        commentAuthor: String,
+        commentEmail: String,
+        commentDate : String
+    }]
+
+});
+
+var Article = module.exports = mongoose.model('Article', articleSchema);
